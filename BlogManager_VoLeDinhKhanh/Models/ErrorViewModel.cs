16cf8d5b6f3e7d4a16b8a849692409c520533f13
Vo Le Dinh Khanh <voledinhkhanh@gmail.com>
2026-07-23 10:14:49 +0700

    first commit

---
+++ BlogManager_VoLeDinhKhanh/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlogManager_VoLeDinhKhanh.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
